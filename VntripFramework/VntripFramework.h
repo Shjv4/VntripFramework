b45008a7d8e78c06f6c05b09884d5937e781b568
HoangNA <shiva8x@gmail.com>
2017-02-23 09:54:33 +0700

    Initial Commit

---
+++ VntripFramework/VntripFramework.h
@@ -0,0 +1,19 @@
+//
+//  VntripFramework.h
+//  VntripFramework
+//
+//  Created by Hoang NA on 2/23/17.
+//  Copyright © 2017 VNTrip OTA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VntripFramework.
+FOUNDATION_EXPORT double VntripFrameworkVersionNumber;
+
+//! Project version string for VntripFramework.
+FOUNDATION_EXPORT const unsigned char VntripFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VntripFramework/PublicHeader.h>
+
+
