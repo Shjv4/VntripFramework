c135ff3b466ef612d5c6a6b21a355e0db44079c8
HoangNA <shiva8x@gmail.com>
2017-02-23 13:05:53 +0700

    update vntripview

--- VntripFramework/Classes/Extensions.swift
+++ VntripFramework/Classes/Extensions.swift
@@ -9,31 +9,31 @@
 import UIKit
 
 extension UIView {
-    @IBInspectable var cornerRadius: CGFloat {
-        get {
-            return self.layer.cornerRadius
-        }
-        set {
-            self.layer.cornerRadius = newValue
-            self.layer.masksToBounds = newValue > 0
-        }
-    }
-    @IBInspectable var borderWidth: CGFloat {
-        get {
-            return self.layer.borderWidth
-        }
-        set {
-            self.layer.borderWidth = newValue
-        }
-    }
-    @IBInspectable var borderColor: CGColor? {
-        get {
-            return self.layer.borderColor
-        }
-        set  {
-            self.layer.borderColor = newValue
-        }
-    }
+//    @IBInspectable var cornerRadius: CGFloat {
+//        get {
+//            return self.layer.cornerRadius
+//        }
+//        set {
+//            self.layer.cornerRadius = newValue
+//            self.layer.masksToBounds = newValue > 0
+//        }
+//    }
+//    @IBInspectable var borderWidth: CGFloat {
+//        get {
+//            return self.layer.borderWidth
+//        }
+//        set {
+//            self.layer.borderWidth = newValue
+//        }
+//    }
+//    @IBInspectable var borderColor: CGColor? {
+//        get {
+//            return self.layer.borderColor
+//        }
+//        set  {
+//            self.layer.borderColor = newValue
+//        }
+//    }
 }
 
 
---
+++ VntripFramework/Classes/VNTView.swift
@@ -0,0 +1,34 @@
+//
+//  VNTView.swift
+//  VntripFramework
+//
+//  Created by Hoang NA on 2/23/17.
+//  Copyright © 2017 VNTrip OTA. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class VNTView: UIView {
+    @IBInspectable var radius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = radius
+            self.layer.masksToBounds = radius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet  {
+            self.layer.borderColor = borderColor?.cgColor
+        }
+    }
+
+}
