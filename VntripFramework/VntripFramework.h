127828047cac67c2808d72e60d56cae590a5a4d8
HoangNA <shiva8x@gmail.com>
2017-02-23 11:33:34 +0700

    update project hierachy

--- VntripFramework/VntripFramework.h
+++ VntripFramework/VntripFramework.h
@@ -17,3 +17,5 @@ FOUNDATION_EXPORT const unsigned char VntripFrameworkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <VntripFramework/PublicHeader.h>
 
 
+
+
